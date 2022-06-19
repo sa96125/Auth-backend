89432af350944bbee5c588eb77b2eefb056a6c89
jongseung park <sa96125@gmail.com>
2022-06-19 22:58:33 +0900

    update : mailService

--- src/@mail/interfaces/mail-module.options.interface.ts
+++ src/@mail/interfaces/mail-module.options.interface.ts
@@ -3,3 +3,8 @@ export class MailModuleOptions {
   domain: string;
   fromEmail: string;
 }
+
+export class EmailVariable {
+  key: string;
+  value: string;
+}
--- src/@mail/mail.service.ts
+++ src/@mail/mail.service.ts
@@ -1,6 +1,9 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { CONFIG_OPTIONS } from '../common/common.constants';
-import { MailModuleOptions } from './interfaces/mail-module.options.interface';
+import {
+  EmailVariable,
+  MailModuleOptions,
+} from './interfaces/mail-module.options.interface';
 import got from 'got';
 import * as FormData from 'form-data';
 
@@ -8,20 +11,26 @@ import * as FormData from 'form-data';
 export class MailService {
   constructor(
     @Inject(CONFIG_OPTIONS) private readonly options: MailModuleOptions,
+  ) {}
+
+  // 서버에서 http 요청 보내기.
+  private async sendEmail(
+    template: string,
+    subject: string,
+    emailVariables: EmailVariable[],
+    to: string,
   ) {
-    this.sendEmail('testing', 'test', 'sa96125@gmail.com');
-  }
-
-  private async sendEmail(subject: string, content: string, to: string) {
     const form = new FormData();
+    form.append('template', template);
     form.append('from', `Excited User <mailgun@${this.options.domain}>`);
-    form.append('to', to);
+    form.append('to', 'sa96125@gmail.com');
     form.append('subject', subject);
-    form.append('text', content);
+    emailVariables.forEach((emailVariable) =>
+      form.append(`v:${emailVariable.key}`, emailVariable.value),
+    );
 
-    const res = await got(
-      `https://api.mailgun.net/v3/${this.options.domain}/messages`,
-      {
+    try {
+      await got(`https://api.mailgun.net/v3/${this.options.domain}/messages`, {
         headers: {
           Authorization: `Basic ${Buffer.from(
             `api:${this.options.apiKey}`,
@@ -29,7 +38,21 @@ export class MailService {
         },
         method: 'POST',
         body: form,
-      },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  sendVerificationEmail(email: string, code: string, to: string) {
+    this.sendEmail(
+      'verify-email',
+      'Verify Your Email',
+      [
+        { key: 'code', value: code },
+        { key: 'username', value: email },
+      ],
+      to,
     );
   }
 }
